5b09667a51ae6065b4d43bf3c4d74a5aac6e5171
Jack <kirbybri@gmail.com>
2016-05-27 12:36:27 -0400

    Update to gain measurement script

--- exampleScripts/doGainMeasurement_extPulser.py
+++ exampleScripts/doGainMeasurement_extPulser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python33
-
+import sys
 import string
 from femb_rootdata import FEMB_ROOTDATA
 from femb_config_35t import FEMB_CONFIG
@@ -10,22 +10,22 @@ femb_rootdata = FEMB_ROOTDATA()
 
 #configure asics
 g=0
-s=2
+s=3
 b=0
 femb_config.configFeAsic(g,s,b)
 
 #initialize generator
-call(["/home/jack/pulserControl/setSquareWave.py","1111","0.100","0"])
+call(["python","/home/jack/pulserControl/setSquareWave.py","1111","0.100","0"])
 
 subrun = 0
-for step in range(0,50,1):
-	freq = 2317
-	amp = int(step)*0.01
-	call(["/home/jack/pulserControl/setSquareWave.py",str(freq),str(amp),"0"])
+for step in range(0,1000,1):
+	freq = 8317
+	amp = int(step)*0.0005
+	call(["python","/home/jack/pulserControl/setSquareWave.py",str(freq),str(amp),"0"])
 	filename = "output_femb_rootdata_gainMeasurement_externalPulser_" + str(femb_rootdata.date) + "_"  + str(subrun) + ".root"
 	print "Recording " + filename
 	femb_rootdata.filename = filename
-	femb_rootdata.numpacketsrecord = 10
+	femb_rootdata.numpacketsrecord = 50
 	femb_rootdata.run = 0
         femb_rootdata.subrun = subrun
         femb_rootdata.runtype = 2
@@ -39,3 +39,5 @@ for step in range(0,50,1):
 	femb_rootdata.record_data_run()
 	#femb_rootdata.record_channel_data(0)
 	subrun = subrun + 1
+
+call(["python","/home/jack/pulserControl/turnOffOutput.py"])
