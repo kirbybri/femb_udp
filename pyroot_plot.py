45d5007126e6b37a0740bbe3d90306418c4bc0a1
Jack <kirbybri@gmail.com>
2016-07-12 15:29:39 -0400

    adding pyroot plotting

---
+++ pyroot_plot.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python33
+
+import sys
+import string
+from femb_udp_cmdline import FEMB_UDP
+import ROOT
+import time
+import socket
+import struct
+import numpy as np
+femb = FEMB_UDP()
+
+
+c = ROOT.TCanvas()
+#h = c.DrawFrame(-1,90,505,150)
+#h.GetXaxis().SetTitle("Channel Number")
+#h.GetYaxis().SetTitle("Waveform")
+#c.Update()
+#c.Modified()
+
+l_first = 1
+
+while 1:
+#for i in range(0,10,1):
+	data = femb.get_data()
+	xpoint = []
+	ypoint = []
+	num =0
+
+
+	for samp in data:
+		chNum = ((samp >> 12 ) & 0xF)
+		sampVal = (samp & 0xFFF)
+		
+		num = float(num)
+		xpoint.append(num)
+		sampVal = float(sampVal)
+		ypoint.append(sampVal)
+		num = num + 1
+
+
+	num = int(num)
+	xpoint = np.array(xpoint)
+        #print xpoint
+	ypoint = np.array(ypoint)
+	#print ypoint
+#        c = ROOT.TCanvas()
+#        h = c.DrawFrame(-1,90,505,150)
+#        h.GetXaxis().SetTitle("Channel Number")
+#        h.GetYaxis().SetTitle("Waveform")
+        g = ROOT.TGraph(num,xpoint,ypoint)
+        #g.SetMarkerStyle(kFullDotMedium)
+	if l_first:
+		
+		#g.Draw("AL")
+		#h = c.DrawFrame(-1,90,505,150)
+		ymin = ROOT.TMath.MinElement(g.GetN(),g.GetY())
+		ymax = ROOT.TMath.MaxElement(g.GetN(),g.GetY())
+		ymiddle = (ymax - ymin)*.5
+		ymin = ymin - ymiddle
+		ymax = ymax + ymiddle
+		h = c.DrawFrame(-1,ymin,505,ymax)
+		l_first = 0
+
+	g.Draw("L")
+	c.Update()
+	c.Modified()
+	#g.Draw("AL")
+        #time.sleep(1)
+
