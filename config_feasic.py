b50acb3f37dd7706de9350eea313f1b5b8ee0d3b
Jack <kirbybri@gmail.com>
2016-07-15 10:02:38 -0400

    changes the number of arguments

--- config_feasic.py
+++ config_feasic.py
@@ -10,21 +10,21 @@ from setup_config import *
 femb_config = config.FEMB_CONFIG()
 
 
-if len(sys.argv) != 4 :
+if len(sys.argv) != 5 :
 	print 'Invalid # of arguments, usage python config_feasic <gain:0-3> <shaping time:0-3> <baseline:0-1>'
 	sys.exit(0)
 
-gainVal = int( sys.argv[1] )
+gainVal = int( sys.argv[2] )
 if (gainVal < 0) or (gainVal > 3):
 	print 'Invalid gain'
         sys.exit(0)
 
-shapeVal = int( sys.argv[2] )
+shapeVal = int( sys.argv[3] )
 if (shapeVal < 0) or (shapeVal > 3):
         print 'Invalid shaping time'
         sys.exit(0)
 
-baseVal = int( sys.argv[3] )
+baseVal = int( sys.argv[4] )
 if (baseVal < 0) or (baseVal > 1):
         print 'Invalid baseline'
         sys.exit(0)
