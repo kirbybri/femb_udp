9e6fb2ca324cedffbb158d353958cc8aadb16fbc
Matt <mworcester@bnl.gov>
2016-08-10 20:00:00 -0500

    make work with sbnd board (3 input instead of 2)

--- select_channel.py
+++ select_channel.py
@@ -15,25 +15,52 @@ config = importlib.import_module(mod)
 
 femb_config = config.FEMB_CONFIG()
 
-
-if len(sys.argv) != 4 :
-	print 'Invalid # of arguments, usage python select_channel <ASIC #> <channel #>'
-	sys.exit(0)
-
-asicVal = int( sys.argv[2] )
-if (asicVal < 0) or (asicVal > 7):
-	print 'Invalid ASIC number'
-        sys.exit(0)
-
-channelVal = int( sys.argv[3] )
-if (channelVal < 0) or (channelVal > 15):
-        print 'Invalid channel number'
-        sys.exit(0)
-
-#femb_config = FEMB_CONFIG()
-
 print "BEGIN CHANNEL SELECT"
-print "ASIC value is", asicVal
-print "Channel value is", channelVal
-femb_config.selectChannel(asicVal,channelVal)
+
+if config_type == 'sbnd':
+
+	if len(sys.argv) != 4 :
+		print 'Invalid # of arguments, usage python select_channel <ASIC #> <channel #> <HS>'
+		sys.exit(0)
+
+	asicVal = int( sys.argv[1] )
+	if (asicVal < 0) or (asicVal > 7):
+		print 'Invalid ASIC number'
+		sys.exit(0)
+
+	channelVal = int( sys.argv[2] )
+	if (channelVal < 0) or (channelVal > 15):
+		print 'Invalid channel number'
+		sys.exit(0)
+
+	hsVal = int( sys.argv[3] )
+	if (hsVal < 0) or (hsVal > 1):
+		print 'Invalid HS'
+		sys.exit(0)
+
+	print "ASIC value is", asicVal
+	print "Channel value is", channelVal
+	print "HS value is",hsVal
+	femb_config.selectChannel(asicVal,channelVal,hsVal)
+
+else:
+
+	if len(sys.argv) != 3 :
+		print 'Invalid # of arguments, usage python select_channel <ASIC #> <channel #>'
+		sys.exit(0)
+
+	asicVal = int( sys.argv[1] )
+	if (asicVal < 0) or (asicVal > 7):
+		print 'Invalid ASIC number'
+		sys.exit(0)
+
+	channelVal = int( sys.argv[2] )
+	if (channelVal < 0) or (channelVal > 15):
+		print 'Invalid channel number'
+		sys.exit(0)
+
+	print "ASIC value is", asicVal
+	print "Channel value is", channelVal
+	femb_config.selectChannel(asicVal,channelVal)
+
 print "END CHANNEL SELECT"
